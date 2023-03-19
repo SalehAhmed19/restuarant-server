82c108c77607ead4c1102185ef298b3e9a611d94
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-03-19 21:08:03 +0600

    integrate jwt token secret

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
+
 const PORT = process.env.PORT || 4000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.accessTokenSecret, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ message: "Access Forbidden" });
+    }
+    console.log("decoded ", decoded);
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.dbUser}:${process.env.dbPassword}@cluster0.f0u4qmq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -26,6 +44,15 @@ async function run() {
       .collection("drinksCollection");
     const mainCollection = client.db("restuarant").collection("mainDishes");
     const cartCollection = client.db("restuarant").collection("cartCollection");
+
+    // authentication token
+    app.post("/api/token", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.accessTokenSecret, {
+        expiresIn: "1d",
+      });
+      res.send({ token: token });
+    });
 
     // get dessert
     app.get("/api/desserts", async (req, res) => {
@@ -89,12 +116,17 @@ async function run() {
     });
 
     // get all cart items api
-    app.get("/api/cart", async (req, res) => {
+    app.get("/api/cart", verifyJwt, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const customerEmail = req.query.customerEmail;
-      const query = { customerEmail: customerEmail };
-      const cursor = cartCollection.find(query);
-      const cart = await cursor.toArray();
-      res.send(cart);
+      if (customerEmail === decodedEmail) {
+        const query = { customerEmail: customerEmail };
+        const cursor = cartCollection.find(query);
+        const cart = await cursor.toArray();
+        res.send(cart);
+      } else {
+        res.status(403).send({ message: "Access Forbidden" });
+      }
     });
 
     app.delete("/api/cart/:id", async (req, res) => {
@@ -125,3 +157,5 @@ app.listen(PORT, () => {
  * app.patch("/api/content/:id") - update content
  * app.delete("/api/content/:id") - delete content
  */
+
+// backend url: https://kayi-tribe-restuarant.onrender.com
