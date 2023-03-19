d3940251b98a2578333f9e001ee0e11e8d980996
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-03-19 23:26:16 +0600

    jwt token updated

--- index.js
+++ index.js
@@ -16,16 +16,29 @@ const verifyJwt = (req, res, next) => {
     return res.status(401).send({ message: "Unauthorized Access" });
   }
   const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.accessTokenSecret, (error, decoded) => {
+  jwt.verify(token, process.env.accessTokenSecret, function (error, decoded) {
     if (error) {
       return res.status(403).send({ message: "Access Forbidden" });
     }
-    console.log("decoded ", decoded);
     req.decoded = decoded;
     next();
-    //
   });
 };
+
+// const verifyJWT = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "Unauthorized" });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden" });
+//     }
+//     req.decoded = decoded;
+//     next();
+//   });
+// };
 
 const uri = `mongodb+srv://${process.env.dbUser}:${process.env.dbPassword}@cluster0.f0u4qmq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
